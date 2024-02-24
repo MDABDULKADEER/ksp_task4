7820f4f8818b94ec325602d12b175770eefe932d
MD ABDUL KADEER <117662992+MDABDULKADEER@users.noreply.github.com>
2024-02-25 00:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/*
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    alert("hello guys");
+</script>
+// linking js and html
+<script src="script.js"></script>
+
+</html>
+*/
+
+//variables
+
+let number1=5;
+console.log(number1);
+
+//var let const
+//let-is for declaring variables
+
+//datatypes
+let number=35;     //integer
+let number2=33.40; //float
+let name1="vinay"; //string
+let character="K"; //char
+let bool=true;     //boolean
+
+//typeof function
+console.log(typeof(name1));
+console.log(typeof(character));
+
+
